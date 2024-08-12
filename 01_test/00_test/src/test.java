c57f063da6c3fb5d26867e54c00739743fe05ac5
wqh <2361125846@qq.com>
2024-08-12 14:40:19 +0800

    更新测试

--- 01_test/00_test/src/test.java
+++ 01_test/00_test/src/test.java
@@ -1,5 +1,17 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.crypto.spec.DESKeySpec;
+
+
+
 public class test {
-    public static void main(String[] args) {
-        String data = {"equtype":"ANDROID","loginImei":"Androidnull","sign":"5D5CB21ADDDF356F84BE528EE31E34AE","timeStamp":"1723276936317","userPwd":"1234567890","username":"15588547880"};
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        String data = "65102933";
+        md.update(data.getBytes());
+        DESKeySpec dsk = new DESKeySpec(md.digest());
+
+        System.out.println(dsk);
     }
 }
