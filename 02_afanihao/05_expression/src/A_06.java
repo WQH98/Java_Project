88f5a50a849299f59918cdaecab1733cb201ceee
WQH98 <2361125846@qq.com>
2024-10-06 08:11:24 +0800

    合并前的提交

--- 02_afanihao/05_expression/src/A_06.java
+++ 02_afanihao/05_expression/src/A_06.java
@@ -3,6 +3,6 @@ public class A_06 {
     public static void main(String[] args) {
         int a = 12;
         int b = 20;
-        System.out.println("result: " +   (a >= 12 && b < 24));
+        System.out.println("result: " + (a >= 12 && b < 24));
     }
 }
